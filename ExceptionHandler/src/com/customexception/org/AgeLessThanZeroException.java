8a4093980a961bdfc19047b750839b9a8a7a1102
delciocapolo <delciocapolo096@gmail.com>
2026-01-21 15:36:38 +0100

    feature: custom exception created and imlemented

---
+++ ExceptionHandler/src/com/customexception/org/AgeLessThanZeroException.java
@@ -0,0 +1,14 @@
+package com.customexception.org;
+
+public class AgeLessThanZeroException extends Exception {
+    public AgeLessThanZeroException() {}
+    public AgeLessThanZeroException(String message) {
+        super(message);
+    }
+    public AgeLessThanZeroException(Throwable cause) {
+        super(cause);
+    }
+    public AgeLessThanZeroException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
