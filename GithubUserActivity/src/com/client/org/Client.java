7291345f6fe38de63590df8c56f6e9af0804342d
delciocapolo <delciocapolo096@gmail.com>
2026-02-02 12:20:45 +0100

    feat: usage HTTPClient library

---
+++ GithubUserActivity/src/com/client/org/Client.java
@@ -0,0 +1,13 @@
+package com.client.org;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+
+public class Client {
+    public void get() throws Exception{
+
+    }
+}
+
+// https://www.baeldung.com/java-9-http-client
